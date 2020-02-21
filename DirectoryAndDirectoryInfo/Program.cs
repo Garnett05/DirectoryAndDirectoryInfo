358c62bf688228090e3cddd6cc0e0fa82a3f6e00
Garnett05 <charlleslimamota10@hotmail.com>
2020-02-21 00:10:03 -0300

    Working with Directory and DirectoryInfo

---
+++ DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace DirectoryAndDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                foreach (string f in files)
+                {
+                    Console.WriteLine(f);
+                }
+                Directory.CreateDirectory(path + "\\newfolder");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+        }
+    }
+}
